106fca9bbf590fe26317c4eea1b3a7fdb89c2f3f
gnujim <mijung.m@gmail.com>
2017-09-11 18:34:53 +0000

    completed step 1

---
+++ step1-printinginchunks.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+const getAndPrintHTMLChunks = () => {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  https.get(requestOptions, response => {
+    response.setEncoding('utf8');
+
+    response.on('data', data => {
+      console.log(data, '\n');
+    });
+
+    response.on('end', () => {
+      console.log('Response completed');
+    });
+  });
+};
+
+getAndPrintHTMLChunks();
